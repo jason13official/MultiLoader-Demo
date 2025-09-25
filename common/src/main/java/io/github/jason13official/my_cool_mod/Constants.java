173ba6e48248c3cd9453b42b9dd44bcca1c31145
jason13official <jason13official@gmail.com>
2025-09-25 13:50:21 -0400

    feat: refactor

---
+++ common/src/main/java/io/github/jason13official/my_cool_mod/Constants.java
@@ -0,0 +1,11 @@
+package io.github.jason13official.my_cool_mod;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "my_cool_mod";
+	public static final String MOD_NAME = "My Cool Mod";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
